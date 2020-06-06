27c928de8b49b8d49817ec1f59da4b831238d06e
moatru <moasteve@gmail.com>
2020-06-06 14:37:56 -0400

    big edits

--- assets/main.js
+++ assets/main.js
@@ -1,34 +1,14 @@
+$( document ).ready(function() {
+   $(".burger-button").click(function(){
+     $(".burger-button").toggleClass("active");
+     $(".burger-menu").slideToggle();
+   });
 
+  $(".burger-menu").click(function(){
+    $(".burger-button").toggleClass("active");
+    $(".burger-menu").slideToggle();
+  });
 
-$(document).ready(function() {
-	//make sure the page is ready befor running the scripts
-	// This is the hamburger menu
-	$(".hamburger").click(function(){
-
-	  $(this).toggleClass('active');
-	  $(".mobile-menu").fadeToggle();
-
-	});
-	// end hamburger menu
-
-	// This is the pop-up gallery
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title');
-			}
-		}
-	});
 
 	var allPanels = $('.accordion > dd').hide();
 
